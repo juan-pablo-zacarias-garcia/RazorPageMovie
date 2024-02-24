70a08bf261c3bcce430617780cd99a9109feb140
juan-pablo-zacarias-garcia <zacarias.garcia.juanpablo.1j@gmail.com>
2024-02-24 13:19:07 -0600

    Agregar archivos de proyecto.

---
+++ RazorPageMovie/Migrations/20240224185456_Rating.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace RazorPageMovie.Migrations
+{
+    /// <inheritdoc />
+    public partial class Rating : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "price",
+                table: "Movie",
+                newName: "Price");
+
+            migrationBuilder.AddColumn<string>(
+                name: "Rating",
+                table: "Movie",
+                type: "nvarchar(max)",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Rating",
+                table: "Movie");
+
+            migrationBuilder.RenameColumn(
+                name: "Price",
+                table: "Movie",
+                newName: "price");
+        }
+    }
+}
---
+++ RazorPageMovie/Models/SeedData.cs
@@ -0,0 +1,62 @@
+﻿using Microsoft.EntityFrameworkCore;
+using RazorPageMovie.Data;
+using System.ComponentModel.DataAnnotations;
+
+namespace RazorPageMovie.Models
+{
+    public class SeedData
+    {
+        public static void Initialize(IServiceProvider serviceProvider)
+        {
+            using (var context = new RazorPageMovieContext(serviceProvider.GetRequiredService<DbContextOptions<RazorPageMovieContext>>()))
+            {
+                if(context == null || context.Movie==null)
+                {
+                    throw new ArgumentException("NullRazorPagesMovieContext");
+                }
+                //Para mirar cualquier película
+                if (context.Movie.Any())
+                {
+                    return;//Db muestra todo lo que contiene la clase
+                }
+                context.Movie.AddRange(new Movie
+                {
+                    Title="When I met your mother",
+                    ReleaseDate=DateTime.Parse("1989-01-25"),
+                    Genre = "Romantic Comedy",
+                    Price = 7.99M,
+                    Rating = "R"
+
+                },
+                new Movie
+                {
+                    Title = "Ghost Busters",
+                    ReleaseDate = DateTime.Parse("1984-03-13"),
+                    Genre = "Comedy",
+                    Price = 8.99M,
+                    Rating = "G"
+                },
+                new Movie
+                {
+                    Title = "Forest Gump",
+                    ReleaseDate = DateTime.Parse("2000-06-25"),
+                    Genre = "Comedy",
+                    Price = 6.78M,
+                    Rating = "G"
+                },
+                new Movie
+                {
+                    Title = "Rio Bravo",
+                    ReleaseDate = DateTime.Parse("1959-04-15"),
+                    Genre = "Western",
+                    Price = 3.99M,
+                    Rating = "NA"
+                }
+                );
+
+                context.SaveChanges();
+            }
+        }
+        
+    }
+}
